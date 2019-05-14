c55a1a9b6621c83c730a0e96cb63b94a06065864
Maxi Wimmer <mc.w1mm3r@gmail.com>
2019-05-14 19:21:42 +0200

    Update batch_test.py

--- 0.4/Tests/batch_test.py
+++ 0.4/Tests/batch_test.py
@@ -5,16 +5,17 @@ from pyglet.window import key
 
 class Window(pyglet.window.Window):
     def __init__(self,*args):
-        super().__init__(*args,resizable=True,vsync=True)
+        super().__init__(*args,resizable=True,vsync=True,)
         self.batch = pyglet.graphics.Batch()
         img = pyglet.image.load('resc\palatin.png')
         self.sprite2 = pyglet.sprite.Sprite(img,100, 0,batch=self.batch)
-        
+        self.set_minimum_size(600,600)
 
     def on_resize(self, width, height):
-        change_width = (width - startwidth) / startwidth + 1
-        width = Window.get_size(self)
-        print(change_width)        
+        changed_width = (width-600)/600+1
+        changed_height = (height-600)/600+1
+        glScalef(changed_width,changed_height,1)
+        print(changed_width)        
 
     def on_draw(self):
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) 
@@ -23,16 +24,8 @@ class Window(pyglet.window.Window):
         self.batch.draw()
         self.sprite2.draw()
 
-
-
     def on_key_press(self, KEY, modifiers):
-        if KEY == key.A:
-           glScalef(2.0, 2.0, 2.0)
-           Window.set_size(self,900,600)
-        if KEY == key.B:
-           glScalef(0.5,0.5,0.5)
-           print('Hallo')
-startwidth = Window.get_size(self)
-print(startwidth)
-window = Window()
+        pass
+
+window = Window(600,600)
 pyglet.app.run()
