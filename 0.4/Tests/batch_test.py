507d796879207e3aecad47d3378517f90f3bdc3b
Haelox <34794208+Haelox@users.noreply.github.com>
2019-05-14 19:54:41 +0200

    deleted multiples

--- 0.4/Tests/batch_test.py
+++ 0.4/Tests/batch_test.py
@@ -17,23 +17,9 @@ class Window(pyglet.window.Window):
     def update(self,dt):
         pass
     
-    def __init__(self,*args):
-        super().__init__(*args,resizable=True,vsync=True,)
-        self.batch = pyglet.graphics.Batch()
-        img = pyglet.image.load('resc\palatin.png')
-        self.sprite2 = pyglet.sprite.Sprite(img,100, 0,batch=self.batch)
-        self.set_minimum_size(600,600)
-
-    def on_resize(self, width, height):
-        changed_width = (width-600)/600+1
-        changed_height = (height-600)/600+1
-        glScalef(changed_width,changed_height,1)
-        print(changed_width)        
-
     def on_draw(self):
         self.clear()
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         self.batch.draw()
         
     def on_resize(self, width, height):
@@ -48,11 +34,3 @@ if __name__ == '__main__':
     window = Window(120,100,"resize_test")
     glClearColor(255,255,255,255)
     pyglet.app.run()
-    self.sprite2.draw()
-
-    def on_key_press(self, KEY, modifiers):
-        pass
-
-window = Window(600,600)
-pyglet.app.run()
-
