7a8326b85c61b0708aa77268d1ff4d27c7291a99
Maxi Wimmer <mc.w1mm3r@gmail.com>
2019-05-23 20:36:02 +0200

    Update Window.py

--- Window.py
+++ Window.py
@@ -1,3 +1,18 @@
+try:
+  import pyglet
+  from pyglet.gl import *
+  import pop_up, Batch, Cards, Card
+  from pyglet.window import key, mouse
+  import chat_dependencies.main_chat as main_chat
+  import random
+except ImportError as err:
+  print("couldn't load modue. %s" % (err))
+
+val = 1
+SPRITE_WIDTH = int(120/val)
+SPRITE_HEIGHT = int(100/val)
+INDENTATION_RIGHT = 2
+
 class Window(main_chat.Window):
   def __init__(self,*args,**kwargs):
     super().__init__(*args,**kwargs)
