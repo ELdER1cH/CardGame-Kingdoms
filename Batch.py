9c9fa936a81dd04e2485e940d3a1fbc3d079ca54
Maxi Wimmer <mc.w1mm3r@gmail.com>
2019-07-06 19:25:39 +0200

    Wall Init Commit

--- Batch.py
+++ Batch.py
@@ -34,7 +34,7 @@ class CardBatch(pyglet.graphics.Batch):
         if i <= 3:
           Card.Card("yellow",i2*120,i*100,batch=self,owner="yellow")
         if i == 4:
-          Card.Card("gray",i2*120,i*100,batch=self,owner="gray")
+          Card.Card("wall",i2*120,i*100,batch=self,owner="gray")
         if i >= 5:
           c = Card.Card("green",i2*120,i*100,batch=self,owner="green")
           c.image.anchor_x = 120; c.image.anchor_y = 100; c.rotation = 180
