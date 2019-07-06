62b5d18fbf2495db4e6f5ab4df8adc51f9d83920
Maxi Wimmer <mc.w1mm3r@gmail.com>
2019-07-06 17:20:25 +0200

    Fixing Screen

--- screens.py
+++ screens.py
@@ -58,30 +58,54 @@ class HandSelection:
                                                      height-(int(i/self.cpr)+1)*(100+self.gap),batch=batch))
             
     def move_card(self,x,y):
+        #rx & ry werden angepasst, dass sie am Rand des Kartenauswahlfelds beginnen und nicht am Rand des Fensters 
         rx = x-self.indentation
         ry = y-(self.h-self.height)
+        #wenn eine Karte der Hand hinzugefügt werden soll - Klick in kartenauswahl  
         if y >= self.h-3*(100+self.gap):
+            # rx wird geteilt - soll maximal 5 (bzw. 0-4) sein, weil es 5 Karten pro Reihe gibt
             rx /= (120+self.gap); ry = (self.height-ry)/(100+self.gap)
+            # wenn der click nicht zwischen den Karten war 
             if rx-int(rx) <= 1-self.gap/(120+self.gap): 
                 if ry-int(ry) >= self.gap/(100+self.gap):
-                    num = int(int(rx) + int(ry)* (self.width/(120+self.gap)-1))
+                    # Testet in welcher Reihe geklickt wurde und setzt je nachdem num fest
+                      # Num Calc: Karten Nummer wird anhand der Stelle und Reihe bestimmt (Zahl von 0-4 + (0-2 für die Reihe * 5 Karten pro Reihe)
+                    #Reihe 1
+                    if int(int(ry)* self.width/(120+self.gap)-1) < 5:
+                      num = int(int(rx) + int(ry)* self.width/(120+self.gap)) 
+                    #Reihe 2
+                    elif   int(int(ry)* self.width/(120+self.gap)-1) < 11:  
+                      num = int(int(rx) + int(ry)* self.width/(120+self.gap)-1)
+                    #Reihe 3
+                    else:
+                      num = int(int(rx) + int(ry)* self.width/(120+self.gap)-2)    
+                    # wenn die Nummer nicht größer als alle Karten ist & und die Nummer noch nicht in die Hand gewählt wurde 
                     if num < len(self.all_cards) and num not in self.hand:
+                        #wenn Die Hand nicht schon voll ist 
                         if len(self.hand) < self.max_hand_lenght:
+                            # Karte nach unten in seinen Platz in der Hand schieben
                             self.sprites[num].y = self.position[1]
                             self.sprites[num].x = (120+self.gap)*len(self.hand)#+self.indentation
                             self.hand.append(num)
-            
+        # wenn der Klick bei den schon ausgewählten Karten war    
         elif y <= self.h-3*(100+self.gap)-self.gap:
+            # num ist wieder die Stelle in der Reihe - bloß gibt es nur eine Reihe
             num = int(x/(120+self.gap))
             rx = x/(120+self.gap)
+            # Lücken Check - soll nicht dazwischen sein 
             if rx-int(rx) >= 1-self.gap/(120+self.gap): 
                 return
+            # wenn alles passt die Karte wieder aus der Hand entfernen und an den alten Platz ziehen     
             if len(self.hand) > num:
                 card = self.hand[num]
                 self.sprites[card].y = self.h-(int(card/self.cpr)+1)*(100+self.gap)
                 self.sprites[card].x = self.indentation+(card % self.cpr)*(120+self.gap)
                 
                 self.hand.remove(card)
+                
+                if len(self.hand) > 0:
+                    for c in self.hand[num:]:
+                        self.sprites[c].x -= (120+self.gap)
                 
                 if len(self.hand) > 0:
                     for c in self.hand[num:]:
