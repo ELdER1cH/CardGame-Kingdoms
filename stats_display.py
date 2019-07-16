6254fded722aa8f11cda14c89a16e0d9ea1d8092
CardGameCommit <mc.w1mm3r@gmail.com>
2019-07-16 12:15:31 +0200

    Card View Implemented

--- stats_display.py
+++ stats_display.py
@@ -8,26 +8,44 @@ class Stats_Display():
         #self.document.set_style(0,len(self.document.text),dict(color=(255,0,0,255)))
         #self.text = pyglet.text.layout.TextLayout(self.document,240,420,multiline=True)
         self.blank = pyglet.sprite.Sprite(pyglet.image.load("resc/jolas/blank_card.png"),135*6, 10)
-        self.select_sprite = pyglet.sprite.Sprite(pyglet.image.load("resc/gray_frame.png"),self.blank.x+100, self.blank.y+300)
+        self.select_sprite = pyglet.sprite.Sprite(pyglet.image.load("resc/gray_frame.png"),self.blank.x+50, self.blank.y+220)
         self.mana_label = pyglet.text.Label("",
                           font_name='Times New Roman',
                           font_size=12,
                           bold=True,color=(109, 43, 43,255),
                           x=720, y=790,
                           anchor_x='center', anchor_y='top',multiline=True,width=220)
-        self.card_label = pyglet.text.Label("",
-                          font_name='Times New Roman',
-                          font_size=12,
-                          bold=True,color=(109, 43, 43,255),
-                          x=720, y=600,
-                          anchor_x='center', anchor_y='top',multiline=True,width=180)
+    # ---------------------------Card Describtion ----------------------------------------
         self.card_describtion_card = pyglet.text.Label("",
                             font_name='Bahnschrift Light', font_size=12,
+                            color=(109, 43, 43,255),
+                            x=self.blank.x+50, y=self.blank.y+160,
+                            anchor_x='left', anchor_y='top',multiline=True,width=265)
+    # ------------------------- Card Name ----------------------------------------------------    
+        self.card_name = pyglet.text.Label("",
+                            font_name='Bahnschrift Light', font_size=24,
                             bold=True,color=(109, 43, 43,255),
-                            x=self.blank.x+150, y=self.blank.y+160,
-                            anchor_x='center', anchor_y='top',multiline=True,width=180)
+                            x=self.blank.x+self.blank.width//2, y=self.blank.y+500,
+                            anchor_x='center', anchor_y='center')
+        
+    # -------------------------- Stats Block ------------------------------------------------------------        
+        self.card_damage = pyglet.text.Label("",
+                            font_name='Bahnschrift Light', font_size=12,
+                            bold = True,color=(109, 43, 43,255),
+                            x=self.blank.x+85, y=self.blank.y+205,
+                            anchor_x='left', anchor_y='top')
+        self.card_health = pyglet.text.Label("",
+                            font_name='Bahnschrift Light', font_size=12,
+                            bold = True,color=(109, 43, 43,255),
+                            x=self.blank.x+200, y=self.blank.y+205,
+                            anchor_x='left', anchor_y='top')
+        self.card_cost = pyglet.text.Label("",
+                            font_name='Bahnschrift Light', font_size=12,
+                            bold = True,color=(109, 43, 43,255),
+                            x=self.blank.x+300, y=self.blank.y+205,
+                            anchor_x='left', anchor_y='top')
     
-        self.blank.x
+        
 
     def update(self,mana,max_mana,mana_reg,target):
         self.mana_label.text = """
@@ -38,15 +56,14 @@ class Stats_Display():
             self.select_sprite.image = pyglet.image.load(target.img[:-4]+"_large.png")
         except:
             self.select_sprite.image = pyglet.image.load(target.img)
-        self.card_label.text = """
-        %s
-        Health: %s
-        Attack: %s
-        Crit: %s
-        Price: %s""" % (target.name,target.health,target.dmg,
-                        target.crit,target.price)
+        
 
         self.card_describtion_card.text = target.description
+        self.card_name.text = target.name
+        self.card_damage.text = str(target.dmg)
+        self.card_health.text = str(target.health)
+        self.card_cost.text = str(target.price)
+        
         
         
     def clear(self):
@@ -68,5 +85,9 @@ class Stats_Display():
         self.mana_label.draw()
         self.blank.draw()
         self.select_sprite.draw()
-        self.card_label.draw()
+        
         self.card_describtion_card.draw()
+        self.card_name.draw()
+        self.card_damage.draw()
+        self.card_health.draw()
+        self.card_cost.draw()
